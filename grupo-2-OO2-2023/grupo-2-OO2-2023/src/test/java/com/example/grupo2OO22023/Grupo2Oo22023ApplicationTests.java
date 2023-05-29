62fc5f0bd7d82f662c0d52aa94fac8255754b46e
AlanChud <105174423+AlanChud@users.noreply.github.com>
2023-05-29 18:01:29 -0300

    Add files via upload
    Creacion del proyecto

---
+++ grupo-2-OO2-2023/grupo-2-OO2-2023/src/test/java/com/example/grupo2OO22023/Grupo2Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo2OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
